a897753cac9b4ffb772f928fa7913d5270d96fdb
Kristián Tkáčik <74569204+KristianTkacik@users.noreply.github.com>
2025-09-26 14:04:57 +0200

    Add KITE API client (#371)

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -17,6 +17,7 @@ package main
 import (
 	"crypto/tls"
 	"flag"
+	"fmt"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
@@ -42,6 +43,7 @@ import (
 	mmv1alpha1 "github.com/konflux-ci/mintmaker/api/v1alpha1"
 	"github.com/konflux-ci/mintmaker/internal/controller"
 	"github.com/konflux-ci/mintmaker/internal/pkg/config"
+	"github.com/konflux-ci/mintmaker/internal/pkg/kite"
 	mintmakermetrics "github.com/konflux-ci/mintmaker/internal/pkg/metrics"
 	// +kubebuilder:scaffold:imports
 )
@@ -68,6 +70,7 @@ func main() {
 	var enableHTTP2 bool
 	var pprofAddr string
 	var tlsOpts []func(*tls.Config)
+	var kiteAPIURL string
 	flag.StringVar(&metricsAddr, "metrics-bind-address", "0", "The address the metrics endpoint binds to. "+
 		"Use :8443 for HTTPS or :8080 for HTTP, or leave as 0 to disable the metrics service.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
@@ -79,6 +82,7 @@ func main() {
 		"If set, the metrics endpoint is served securely via HTTPS. Use --metrics-secure=false to use HTTP instead.")
 	flag.BoolVar(&enableHTTP2, "enable-http2", false,
 		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
+	flag.StringVar(&kiteAPIURL, "kite-api-url", "", "KITE API base URL")
 	opts := zap.Options{
 		Development: false,
 	}
@@ -172,6 +176,20 @@ func main() {
 		}()
 	} else if pprofAddr != "" {
 		setupLog.Info("pprof server disabled", "reason", "ENABLE_PPROFLING env var not set to 'true'")
+	}
+
+	// Create KITE client
+	kiteClient, err := kite.NewClient(kiteAPIURL)
+	if err != nil {
+		setupLog.Error(err, "failed to create KITE API client")
+	} else {
+		// Log KITE API version
+		ver, err := kiteClient.GetVersion(ctx)
+		if err != nil {
+			setupLog.Error(err, "failed to get KITE API version")
+		} else {
+			setupLog.Info(fmt.Sprintf("KITE API version: %s", ver))
+		}
 	}
 
 	if err = (&controller.DependencyUpdateCheckReconciler{
